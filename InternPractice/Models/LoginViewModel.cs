0a195157f18c7d9daf0b6f73595ca1a0bb634ca1
SidraChatha <146321646+SidraChatha@users.noreply.github.com>
2026-04-27 16:21:58 +0500

    Refactor Identity UI and implement custom Account Controller routing

--- InternPractice/Models/LoginViewModel.cs
+++ InternPractice/Models/LoginViewModel.cs
@@ -4,13 +4,15 @@ namespace InternPractice.Models
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage = "Email is required")]
-        [EmailAddress(ErrorMessage = "Invalid email format")]
-        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress(ErrorMessage = "The email format is not valid")]
+        [Display(Name = "Email Address")]
+        public required string Email { get; set; }
 
-        [Required(ErrorMessage = "Password is required")]
+        [Required(ErrorMessage = "Please enter your password")]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        [Display(Name = "Password")]
+        public required string Password { get; set; }
 
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
---
+++ InternPractice/Models/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InternPractice.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        [Display(Name = "Email Address")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public required string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public required string ConfirmPassword { get; set; }
+    }
+}
